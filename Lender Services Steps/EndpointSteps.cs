4623373af4d66d0a5b9d6e6b7f599905eed9b1b1
Joshua Hughes <joshua.hughes@ivendi.com>
2022-11-30 16:37:24 +0000

    updated step files

---
+++ Lender Services Steps/EndpointSteps.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace apiPrepTestingFramework.QA.Lender_Services_Steps
+{
+    internal class EndpointSteps
+    {
+    }
+}
